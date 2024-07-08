37c909c003321e0d292c7fc971c5638c23ef4a6a
MRY <101030920+MRYcn@users.noreply.github.com>
2024-07-08 15:29:43 +0800

    Update project_game.py

--- Project/project_game.py
+++ Project/project_game.py
@@ -7,29 +7,37 @@ import pygame.font
 import sys
 
 class Pro_game:
-    def __init__(self):
-        pygame.init()
-        self.screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-        self.screen_width=self.screen.get_rect().width
-        self.screen_height=self.screen.get_rect().height
-        pygame.screen.set_caption('Project')
-    
-    def run_game(self):
-        while True:
-            self._check_event()
-            
-            self._update_screen()
-    
-    def _check_event(self):
-        for event in pygame.event.get():
-            if event.type in (pygame.QUIT,pygame.KEY_Q,pygame.KEY_q):
-                if self.quitdef():
-                    pygame.quit()
-                    sys.quit()
-    
-    def _update_screen(self):
-        self.screen.fill((0,0,0))
-        pygame.display.flip()
+	def __init__(self):
+		pygame.init()
+		#self.screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+		self.screen=pygame.display.set_mode((500,500))
+		self.screen_width=self.screen.get_rect().width
+		self.screen_height=self.screen.get_rect().height
+		pygame.display.set_caption('Project')
+	
+	def run_game(self):
+		while True:
+			self._check_event()
+			
+			self._update_screen()
+	
+	def _check_event(self):
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				if self.quitdef():
+					pygame.quit()
+					sys.exit()
+			if event.type==pygame.KEYDOWN and event.key==pygame.K_q:
+								if self.quitdef():
+										pygame.quit()
+										sys.exit()
+	
+	def _update_screen(self):
+		self.screen.fill((0,0,0))
+		pygame.display.flip()
+
+	def quitdef(self):
+		return True
 
 game=Pro_game()
 game.run_game()
