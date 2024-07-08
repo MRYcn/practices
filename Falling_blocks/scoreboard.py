5a650ff1d77bc57f5797f9da0631792640e74bfa
MRY <101030920+MRYcn@users.noreply.github.com>
2024-07-08 15:27:52 +0800

    Update scoreboard.py

--- Falling_blocks/scoreboard.py
+++ Falling_blocks/scoreboard.py
@@ -21,7 +21,7 @@ class Scoreboard:
 		self.limit_life=B_game.stats.lives_limit
 		stats=B_game.stats
 		self.rects['miss']=[]
-		for i in range(self.limit_life-stats.lives.left):
+		for i in range(self.limit_life-stats.lives_left):
 			miss_image=self.font.render('×',True,self.red,(230,230,230))
 			miss_image_rect=miss_image.get_rect()
 			miss_image_rect.centerx=25
@@ -29,7 +29,7 @@ class Scoreboard:
 			comb=[miss_image,miss_image_rect]
 			self.rects['miss'].append(comb)
 			self.miss_y+=20
-		for i in range(stats.lives.left):
+		for i in range(stats.lives_left):
 			miss_image=self.font.render('×',True,self.color,(230,230,230))
 			miss_image_rect=miss_image.get_rect()
 			miss_image_rect.centerx=25
@@ -53,6 +53,8 @@ class Scoreboard:
 		self.rects['highest'].append(comb)
 	def blitme(self,B_game):
 		self._update_sb(B_game)
-		for sect in self.rects:
+		for sect in self.rects.values():
+			print(sect)
 			for rect in sect:
+				print(rect)
 				self.screen.blit(rect[0],rect[1])
