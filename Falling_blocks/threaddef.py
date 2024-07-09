39fe892fd1640ca0477c495342dea941e00e60d0
MRY <101030920+MRYcn@users.noreply.github.com>
2024-07-09 19:04:06 +0800

    更新 threaddef.py

--- Falling_blocks/threaddef.py
+++ Falling_blocks/threaddef.py
@@ -1,6 +1,6 @@
 #coding=utf-8
 #author=MRY
-#releasedtime=
+#releasedtime=2024.7.9
 
 #assist for: block_game.py
 try:
@@ -11,10 +11,11 @@ except:
 class Thread:
 	def __init__(self,B_game):
 		self.screen=B_game.screen
+		self.bg_color=B_game.settings.bg_color
 		self.boot=True
 		self.start=(590,550)
 		self.end=(610,550)
-		self.width=20
+		self.width=8
 		self.length_r=20
 		self.color=(30,30,30)
 	
@@ -31,14 +32,15 @@ class Thread:
 			d=d2-d1
 			dm=d.microseconds/1000
 			if dm>=20:
-				print(self.start)
 				self.start=(self.start[0]-self.length_r,self.start[1])
 				self.end=(self.end[0]+self.length_r,self.end[1])
+				self.screen.fill(self.bg_color)
 				pygame.draw.line(self.screen,self.color,self.start,self.end,self.width)
+				pygame.display.flip()
 				d1=d2
-			if self.start<=60:
+			if self.start[0]<=60:
 				break
 	def reset(self):
-		self.start=590
-		self.end=610
+		self.start=(590,550)
+		self.end=(610,550)
 		self.boot=True
